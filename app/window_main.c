a7a3f345b7daafc5e84825cabe36d04e2bbc3cdb
zjm1060 <zjm1060@163.com>
2020-11-10 10:10:43 +0800

    sdl with opengl

---
+++ app/window_main.c
@@ -0,0 +1,21 @@
+﻿#include "awtk.h"
+
+/**
+ * 初始化
+ */
+ret_t application_init(void) {
+
+  widget_t* system_bar = window_open("system_bar");
+  widget_t* system_bar_b = window_open("system_bar_b");
+  widget_t* win = window_open("home_page");
+
+  return RET_OK;
+}
+
+/**
+ * 退出
+ */
+ret_t application_exit(void) {
+  log_debug("application_exit\n");
+  return RET_OK;
+}
